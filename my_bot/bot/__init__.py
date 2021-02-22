10239b5783d73f158e87eb221e2a41dbe553b976
_psycho_ <mike.belous@gmail.com>
2021-02-22 16:45:53 +0300

    run v.2

--- my_bot/bot/__init__.py
+++ my_bot/bot/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-from ._bot import bot, dp
+from ._bot import bot, dp, WEBHOOK_PATH, on_startup, on_shutdown, WEBAPP_HOST, WEBAPP_PORT, start_webhook
 
-__all__ = ['bot', 'dp']
+__all__ = ['bot', 'dp', 'WEBHOOK_PATH', 'on_startup', 'on_shutdown', 'WEBAPP_HOST', 'WEBAPP_PORT', 'start_webhook']
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from my_bot.bot import bot, dp
-from aiogram import executor
+from my_bot.bot import dp, WEBAPP_PORT, on_startup, on_shutdown, WEBAPP_HOST, WEBHOOK_PATH
+from aiogram.utils.executor import start_webhook
 
 if __name__ == "__main__":
-    executor.start_webhook()
+    start_webhook(dp, WEBHOOK_PATH, on_startup, on_shutdown, True, WEBAPP_HOST, WEBAPP_PORT)
