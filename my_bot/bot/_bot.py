1e244211e20895f00fafce1921a0a576300df72b
PsychoBel <45608177+PsychoBel@users.noreply.github.com>
2021-02-28 12:31:26 +0300

    Update _bot.py

--- my_bot/bot/_bot.py
+++ my_bot/bot/_bot.py
@@ -7,30 +7,20 @@ from my_bot.model import FullModel, transform
 import os
 import cv2
 
-
-
-# Set API_TOKEN. You must have your own.
 BOT_TOKEN = os.environ['BOT_TOKEN']
-
-# webhook settings
 WEBHOOK_HOST = os.environ['WEBHOOK_HOST_ADDR']
 WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
-
-# webserver settings
-WEBAPP_HOST = '0.0.0.0'  # or ip
+WEBAPP_HOST = '0.0.0.0'  
 WEBAPP_PORT = os.environ['PORT']
-# Configure logging.
 logging.basicConfig(level=logging.INFO)
 
-# Initialize bot and dispatcher.
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 buttons_for_start = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 buttons_for_start.add(types.KeyboardButton(text="What can you do? \U0001F9D0"))
 buttons_for_start.add(types.KeyboardButton(text="Tell me about your creator \U0001F468\U0000200D\U0001F4BB"))
-#buttons_for_start.add(types.KeyboardButton(text="What is the weather now? \U00002600"))
 
 
 @dp.message_handler(commands=['start'], state='*')
